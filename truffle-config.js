6837eeae3df852283e57c27c2f81caad1f679917
unknown <pariveshkhandel@gmail.com>
2022-02-26 01:58:18 +0530

    all files

---
+++ truffle-config.js
@@ -0,0 +1,23 @@
+var TestRPC = require("ethereumjs-testrpc");
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8000,
+      network_id: "*" // Match any network id
+    },
+    // add a new network definition that will self host TestRPC
+    localtest: {
+      provider: TestRPC.provider(),
+      network_id:"*"
+    }
+  },
+  // add a section for mocha defaults
+  mocha: {
+    reporter: "spec",
+    reporterOptions: {
+      mochaFile: 'TEST-truffle.xml'
+    }
+  }
+};
