70cc07a5e35f8affe93f6612d5ec6ec1a554834a
techiegk <113419517+techiegk@users.noreply.github.com>
2022-09-13 20:19:51 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -4,11 +4,6 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    },  
-   soliditycoverage: {
-      host: "127.0.0.1",
-      port: 8555,
-      network_id: "*" // Match any network id
     },
     // add a new network definition that will self host TestRP
   },
